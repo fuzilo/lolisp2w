68e3d7f9e6a4a7d51b07515722b63fdc6d24ff72
Ricardo Matsumura <fuzilo@hotmail.com>
2024-07-17 18:02:26 -0300

    aceite de modificações

--- app/app.js
+++ app/app.js
@@ -3,8 +3,8 @@ const connectDB = require('./config/db');
 const skinRoutes = require('./routes/skinRoutes');
 require('dotenv').config();
 const { swaggerUi, swaggerDocs } = require('./src/swagger');
-const logger = require('./src/logger')
-const morgan = require('morgan')
+// const logger = require('./src/logger')
+// const morgan = require('morgan')
 
 const app = express();
 
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json());
 
 // Configurar Morgan para usar o logger do Winston
-app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
+// app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
 
 
 // Rotas
--- app/src/logger.js
+++ app/src/logger.js
@@ -20,4 +20,4 @@ const logger = createLogger({
     ]
 });
 
-module.exports = logger;
+// module.exports = logger;
