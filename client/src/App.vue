1026912a675c5904e77a8d0ecbaef30658c903f2
Italo Costa <italocostajobs@gmail.com>
2024-07-17 13:36:30 -0300

    Feat: navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,23 @@
-<script setup>
+<script>
   import { RouterView } from 'vue-router'
   import SearchBar from './components/SearchBar.vue'
   import HomeButton from './components/HomeButton.vue'
+  export default {
+    name: 'App',
+    components: {
+      RouterView,
+      SearchBar,
+      HomeButton,
+    },
+  };
+
 </script>
 
 <template>
-  <div id="home-bg" class="w-screen h-screen">
-    <nav class="py-2 bg-blend-normal">
-      <div class="mx-10 flex flex-row items-center justify-between">
+  <div id="home-bg" class="w-screen h-screen bg-blue-400">
+    <nav class="py-4 flex flex-row justify-center items-center">
+      <div
+        class="mx-10 py-2 px-4 w-3/5 flex flex-row items-center justify-between bg-blend-normal bg-white-300 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 bg-white/20">
         <HomeButton />
         <SearchBar />
       </div>
@@ -33,9 +43,7 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background-image: url('@/assets/img/logo-v2.jpg');
     background-size: 100% 100%;
-    filter: blur(5px);
-
+    filter: blur(1px);
   }
 </style>
--- client/src/components/SearchBar.vue
+++ client/src/components/SearchBar.vue
@@ -1,36 +1,44 @@
 <template>
     <div class="flex items-center">
-        <input
-            type="text"
-            v-model="searchTerm"
-            placeholder="Search..."
-            class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <button
-            @click="search"
-            class="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-            Search
+        <input type="text" v-model="searchTerm" placeholder="Search..."
+            class="px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        <button @click="search"
+            class="ml-2 px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
         </button>
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            searchTerm: '',
-        };
-    },
-    methods: {
-        search() {
-            // Implement your search logic here
-            console.log('Searching for:', this.searchTerm);
-        },
-    },
-};
+    import axios from 'axios';
+    import { ref } from 'vue';
+
+    export default {
+        setup() {
+            const searchTerm = ref('');
+            const search = () => {
+                axios.get(`http://localhost:8080/skins/search?field=champion&value=${searchTerm.value}`)
+                    .then((response) => {
+                        console.log(response.data);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            };
+
+            return {
+                searchTerm,
+                search,
+            };
+
+        }
+    };
 </script>
 
 <style scoped>
-/* Add any custom styles here */
+    /* Add any custom styles here */
 </style>
